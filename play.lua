6b6d13bb0f2e021ee3dd56cd798773b3b46132b2
Stephen Altamirano <stephen@evilrobotstuff.com>
2013-01-26 20:02:45 -0800

    Adds seperate gui object

--- play.lua
+++ play.lua
@@ -3,6 +3,7 @@ local play = state_manager:register('play')
 
 local SimpleQueue = require 'simple_queue'
 local timer_obj = require 'timer'
+local gui = require 'gui_overlay'
 
 local stages = {
   {
@@ -277,29 +278,7 @@ function play:draw()
 
   love.graphics.pop()
 
-  -- overview bar
-  love.graphics.setColor(41, 39, 59)
-  love.graphics.rectangle(
-    'fill',
-    0, 0,
-    SCREEN_WIDTH, OVERVIEW_HEIGHT
-  )
-
-  -- timer bar
-  love.graphics.setColor(0, 0, 0)
-  love.graphics.rectangle(
-    'fill',
-    0, SCREEN_HEIGHT - TIMER_HEIGHT,
-    SCREEN_WIDTH, TIMER_HEIGHT
-  )
-
-  local timer_progress = timer_obj:percent_remaining()
-  love.graphics.setColor(255, 255 * timer_progress, 41)
-  love.graphics.rectangle(
-    'fill',
-    0, SCREEN_HEIGHT - TIMER_HEIGHT,
-    SCREEN_WIDTH * timer_progress, TIMER_HEIGHT
-  )
+  gui:draw()
 end
 
 return play
