a8b636777239f4edc6d369cbe6eaf6b473a2007b
Stephen Altamirano <stephen@evilrobotstuff.com>
2013-01-27 14:37:56 -0800

    Adds win awesomeness

--- win.lua
+++ win.lua
@@ -2,12 +2,14 @@ local state_manager = require 'state_manager'
 local win = state_manager:register('win')
 local paper = require 'paper'
 local gui = require 'gui_overlay'
+local image_bank = require 'image_bank'
 
 local displacement = 0
 local captured_scene
 local base_y = SCREEN_PADDING + PIN_WIDTH / 2 + 5 * PIN_SPACING - RESTING_PIN_OFFSET
 local points = {600,-600,600,-500,250,-200,100,-90,-80,-80,0,0,0,0,0,0,0,0}
 local dx = 0
+local img = image_bank:get('assets/ggj_win.png')
 
 function win:enter(scene)
   points = {0,0,0,0,0,0,0}
@@ -50,6 +52,12 @@ function win:update(dt)
   paper:update(dt)
 end
 
+function win:keyreleased(k)
+  if k == 'escape' then
+    state_manager:switch('title')
+  end
+end
+
 function win:draw()
   paper:draw_background()
 
@@ -64,7 +72,7 @@ function win:draw()
   else
     love.graphics.setColor(255, 0, 0)
     love.graphics.line(
-      0, base_y,
+      -displacement - SCREEN_WIDTH - 250, base_y,
       -displacement - SCREEN_WIDTH - 250, base_y
     )
   end
@@ -78,6 +86,15 @@ function win:draw()
   end
   love.graphics.line(draw_shit)
   love.graphics.setLineWidth(1)
+
+  local img_width = img:getWidth()
+  local x = -displacement - SCREEN_WIDTH - 250 - img_width * 2
+  love.graphics.setColor(244, 242, 216)
+  love.graphics.setColor(255, 255, 255)
+  love.graphics.draw(
+    img,
+    math.min(x, (SCREEN_WIDTH - img_width) / 2), 80
+  )
 
   gui:draw()
 end
