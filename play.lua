f627fbe596c30230eb2777ed3c437eb616b9c6be
Stephen Altamirano <stephen@evilrobotstuff.com>
2013-01-26 18:15:38 -0800

    Add grid lines

--- play.lua
+++ play.lua
@@ -98,6 +98,23 @@ end
 function play:draw()
   local x, y = 0, 0
 
+  -- grid lines
+  love.graphics.setColor(214, 227, 218)
+  love.graphics.setLineWidth(2)
+  for i=1,SCREEN_HEIGHT / PIN_DY do
+    love.graphics.line(
+      0, (i - 1) * PIN_DY - 10,
+      SCREEN_WIDTH, (i - 1) * PIN_DY - 10
+    )
+  end
+
+  for i=-1,SCREEN_WIDTH / PIN_DY do
+    love.graphics.line(
+      (i-1) * PIN_DY + frac * PIN_SPACING, 0,
+      (i-1) * PIN_DY + frac * PIN_SPACING, SCREEN_HEIGHT
+    )
+  end
+
   -- shear line
   love.graphics.setColor(88, 86, 131)
   love.graphics.setLineWidth(4)
